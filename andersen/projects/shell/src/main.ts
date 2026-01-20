0b9261530abbe557e9039592980c17639afd93f0
TeaChareqishvili <teachareqishvili89@gmail.com>
2026-01-20 17:54:26 +0400

    update versions

--- andersen/projects/shell/src/main.ts
+++ andersen/projects/shell/src/main.ts
@@ -1,6 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, Title } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  bootstrapApplication(AppComponent, appConfig).then(appRef => {
+  const title = appRef.injector.get(Title);
+  title.setTitle('Shell App');
+});
