1e39d518ee652eb9749ebec3b7695f742511e98f
TeaChareqishvili <teachareqishvili89@gmail.com>
2026-01-22 02:10:45 +0400

    fix naviagtion

--- andersen/projects/auth/src/app/app.routes.ts
+++ andersen/projects/auth/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/login/login';
 import { RegisterComponent } from './pages/register/register';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
