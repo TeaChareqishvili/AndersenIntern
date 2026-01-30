d03cfd9acd86690b15160640a41aacc5c233ec6b
YACHNIKMIKHAIL <92398592+YACHNIKMIKHAIL@users.noreply.github.com>
2026-01-30 13:36:13 +0100

    T (#3)
    * T
    
    * storage refactor
    
    * T
    
    ---------
    
    Co-authored-by: TeaChareqishvili <teachareqishvili89@gmail.com>

--- andersen/projects/auth/src/app/app.ts
+++ andersen/projects/auth/src/app/app.ts
@@ -46,7 +46,6 @@ export class App {
       .pipe(
         tap(() => {
           this.authService.setUser(null);
-          this.storage.clearData();
         }),
 
         switchMap(() =>
--- andersen/projects/auth/src/app/services/storage-service/storage-service.service.ts
+++ andersen/projects/auth/src/app/services/storage-service/storage-service.service.ts
@@ -2,39 +2,40 @@ import { Injectable } from '@angular/core';
 import { SessionState } from '../../models/auth.models';
 
 export interface StorageState<T extends object> {
-  getData(): Promise<T>;
-  setData(updater: (state: T) => T): Promise<void>;
-  clearData(): Promise<void>;
+  getItem(key: string): T | null;
+
+  setItem(key: string, value: T): void;
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class StorageService implements StorageState<SessionState> {
-  private readonly STORAGE_KEY = 'APP_SESSION_STATE';
-
-  async getData(): Promise<SessionState> {
-    const raw = localStorage.getItem(this.STORAGE_KEY);
+  getItem<T>(key: string): T | null {
+    const raw = localStorage.getItem(key);
 
     if (!raw) {
-      return {};
+      return null;
     }
 
     try {
-      return JSON.parse(raw) as SessionState;
+      return JSON.parse(raw) as T;
     } catch (err) {
-      return {};
+      console.log(`Error: ${err} in storage service with the ${key} key`);
+      return null;
     }
   }
 
-  async setData(updater: (state: SessionState) => SessionState): Promise<void> {
-    const currentState = await this.getData();
-    const nextState = updater(currentState);
+  setItem<T>(key: string, value: T): void {
+    try {
+      if (value === null) {
+        localStorage.removeItem(key);
+        return;
+      }
 
-    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(nextState));
-  }
-
-  async clearData(): Promise<void> {
-    localStorage.removeItem(this.STORAGE_KEY);
+      localStorage?.setItem(key, JSON.stringify(value));
+    } catch {
+      console.error('Hello world');
+    }
   }
 }
